a3259f9d5c574d1b99c76b09908832132917a244
Ricardo Pietrobon <rpietro@duke.edu>
2012-06-11 03:54:58 -0400

    first commit

---
+++ GameTimePerception.R
@@ -0,0 +1,31 @@
+#######################################################################################
+#template_secondary_data_analysis.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. see full license at the end of this file.
+#######################################################################################
+#this script follows a combination of the guidelines proposed by Hadley Wickham http://goo.gl/c04kq as well as using the formatR package http://goo.gl/ri6ky
+
+#link to manuscript
+
+#####################################################################################
+#SETTING ENVIRONMENT
+#####################################################################################
+#remove all objects and then check
+rm(list = ls())
+ls()
+#dettach all packages
+detach()
+
+#command below will install all packages and is only run once. remove the #if this is the first time you are running the code on RStudio, and then you can add the hash tag again
+#lapply(c("ggplot2", "psych", "RCurl", "irr", "car","Hmisc", "gmodels", "DAAG"), install.packages, character.only=T)
+
+lapply(c("ggplot2", "psych", "RCurl", "irr", "car","Hmisc", "gmodels","qpcR"), library, character.only=T)
+
+#####################################################################################
+#IMPORTING DATA
+#####################################################################################
+
+#if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object
+templateData <- read.csv("")
+
+#######################################################################################
+#template_secondary_data_analysis.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. You are free: to Share — to copy, distribute and transmit the work to Remix — to adapt the work, under the following conditions: Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). Noncommercial — You may not use this work for commercial purposes. With the understanding that: Waiver — Any of the above conditions can be waived if you get permission from the copyright holder. Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license. Other Rights — In no way are any of the following rights affected by the license: Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations; The author's moral rights; Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights. Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page. For more details see http://creativecommons.org/licenses/by-nc/3.0/
+#######################################################################################
