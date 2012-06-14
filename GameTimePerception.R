cfab59bf7ee2ed1b6df2e12b8f0e603759c353b5
Ricardo Pietrobon <rpietro@duke.edu>
2012-06-14 06:19:20 -0400

    everything ready to go prior to question diagram

--- GameTimePerception.R
+++ GameTimePerception.R
@@ -24,7 +24,53 @@ lapply(c("ggplot2", "psych", "RCurl", "irr", "car","Hmisc", "gmodels","qpcR"), l
 #####################################################################################
 
 #if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object
-templateData <- read.csv("/Users/rpietro/Google Drive/R/nonpublicdata_publications/GameTimePerception/gameTimePerception.csv")
+game_time_data <- read.csv("/Users/rpietro/Google Drive/R/nonpublicdata_publications/GameTimePerception/gameTimePerception.csv")
+
+
+#below will view data in a spreadsheet format. notice that in this all subsequent commands you have to replace templateData with whatever name you chose for your data object in the previous command
+
+#View(templateData)
+
+#below will list variable names, classes (integer, factor, etc), alternative responses
+str(game_time_data)
+#list variable names so that they can be used later
+names(game_time_data)
+#below will attach your data so that when you execute a command you don't have to write the name of your data object over and over again
+attach(game_time_data)
+
+#function below is used to recode variables. things to notice: replace old.var with the variable you are recoding, replace new.var with the variable you want to create. the whole recoding happens within " ". all character and factor variables will be within '', numbers will be displayed with digits (not inside '') or NA (also without '')
+new.var  <- car::recode(old.var, " c(1,2) = 'A'; else = 'B' ")
+
+###########################################################################################
+#TABLE 1: DEMOGRAPHICS
+###########################################################################################
+#describes your entire dataset
+describe(templateData)
+
+summary(variable)
+qplot(variable)
+
+#t.test, where outcome is a continuous variable and predictor is a dichotomous variable
+t.test(outcome~predictor)
+
+#chi square test where both outcome and predictor are categorical variables
+CrossTable(outcome, predictor, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
+
+
+########################################################################################
+# Table with models
+########################################################################################
+
+logisticmodel  <- glm(outcome ~ predictor + confounder,family=binomial(link="logit"))
+summary(logisticmodel) #gives you model results
+coefficients(logisticmodel) # model coefficients
+confint(logisticmodel, level=0.95) # CIs for model parameters 
+fitted(logisticmodel) # predicted values
+residuals(logisticmodel) # residuals
+influence(logisticmodel) # regression diagnostics
+layout(matrix(c(1,2,3,4),2,2)) # creates the white space for 4 graphs/page 
+plot(logisticmodel) #generates 4 graphs/page
+
 
 #######################################################################################
 #template_secondary_data_analysis.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. You are free: to Share — to copy, distribute and transmit the work to Remix — to adapt the work, under the following conditions: Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). Noncommercial — You may not use this work for commercial purposes. With the understanding that: Waiver — Any of the above conditions can be waived if you get permission from the copyright holder. Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license. Other Rights — In no way are any of the following rights affected by the license: Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations; The author's moral rights; Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights. Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page. For more details see http://creativecommons.org/licenses/by-nc/3.0/
